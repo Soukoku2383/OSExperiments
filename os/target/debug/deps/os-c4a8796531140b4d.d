c16c722ec687d5d3c24eba8fc5ac7d3a820143bb
soukoku2383 <20301143@bjtu.edu.cn>
2022-11-08 10:58:53 +0800

    20301143万婷钰

---
+++ os/target/debug/deps/os-c4a8796531140b4d.d
@@ -0,0 +1,5 @@
+/mnt/os/target/debug/deps/os-c4a8796531140b4d: src/main.rs
+
+/mnt/os/target/debug/deps/os-c4a8796531140b4d.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d
@@ -0,0 +1,5 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a: src/main.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d: src/main.rs
+
+src/main.rs:
