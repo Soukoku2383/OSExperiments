9aa2c64dd8f8e15947eaec4e08bd412aafa08489
soukoku2383 <20301143@bjtu.edu.cn>
2022-11-08 12:03:00 +0800

    20301143万婷钰

--- os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d
@@ -1,5 +1,9 @@
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-5f5e41097378db9a.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
 src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
